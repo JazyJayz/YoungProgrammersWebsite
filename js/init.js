f10b1c85f804d947e6d7c04ee45d674f7479f2ac
JazyJayz <56541108+JazyJayz@users.noreply.github.com>
2019-10-16 16:16:34 +0330

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,9 +1,3 @@
-/*
-	Telephasic 1.1 by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 skel.init({
 	/*
 		Docs @ http://skeljs.org/docs
